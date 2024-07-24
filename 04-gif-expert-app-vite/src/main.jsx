53edc5cf6fc9a7027e9f92d9d65edfa74931aa0e
Daniel Fragoso <carlos.fragoso@amigodoctor.com>
2024-07-24 17:00:20 -0600

    Lista categorias

--- 04-gif-expert-app-vite/src/main.jsx
+++ 04-gif-expert-app-vite/src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {GifExpertApp} from './GifExpertApp';
+import './style.css';
+
+
+//https://es.legacy.reactjs.org/docs/faq-structure.html
+//https://es.legacy.reactjs.org/docs/faq-structure.html
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GifExpertApp/>
