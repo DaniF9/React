dbe1ab6b9a11c1fec3526e6d0c0b5d57add06dc3
DaniF9 <danib9909@gmail.com>
2024-07-24 23:42:50 -0600

    Add category start 78

--- 04-gif-expert-app-vite/src/GifExpertApp.jsx
+++ 04-gif-expert-app-vite/src/GifExpertApp.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react" //Esto es para utilizar el usestate
 
 
 
 export const GifExpertApp = () => {
+
+  // cuando queremos almacenar info y esa cambia el html necesitamos usar un hook
+  const [categories, setCategories]=useState(['One Punch','Dragon Ball']); 
+  //console.log(categories);
+
+  const addCategory = () => {
+    if (!categories.includes('Valorant')) {
+      setCategories([...categories,'Valorant']);
+    }
+    else{
+      alert("Ya haz agregado esa categoria");
+    }
+    
+  }
   return (
     <>
+    {/* Titulo */}
       <h1>GiftExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de gif */}
+      <button onClick={addCategory}>Agregar Categoria</button>
+      <ol>
+        {categories.map(category => {
+          return <li key={category}>{category}</li>
+          })
+          
+        }
+      </ol>
+          {/* Gif item */}
     </>
   )
 }
